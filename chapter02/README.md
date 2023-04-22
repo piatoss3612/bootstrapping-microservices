545e74093c91f9a382500a692b822d1769185a2c
Lee Hyohwak <crewe1746@naver.com>
2023-04-22 23:35:20 +0900

    chapter02 - simple microservice with node.js

---
+++ chapter02/README.md
@@ -0,0 +1,19 @@
+# Chapter02
+
+## Install Dependencies
+
+```sh
+$ npm i
+```
+
+## Run the application
+
+```sh
+$ npm start
+```
+
+or
+
+```sh
+$ npm run start:dev # live reload
+```
---
+++ chapter02/index.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+
+const express = require("express");
+const fs = require("fs");
+
+const app = express();
+
+if (!process.env.PORT) {
+  throw new Error(
+    "Please specify the port number for the HTTP server with the environment variable PORT."
+  );
+}
+
+const port = process.env.PORT;
+
+app.get("/video", (req, res) => {
+  const path = "./videos/SampleVideo_1280x720_1mb.mp4";
+
+  fs.stat(path, (err, stats) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).send(err.message);
+    }
+
+    res.writeHead(200, {
+      "Content-Length": stats.size,
+      "Content-Type": "video/mp4",
+    });
+    fs.createReadStream(path).pipe(res);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
