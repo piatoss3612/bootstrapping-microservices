3ad5e740a0529030c0a696fc6a3f8f7a821349ed
Lee Hyohwak <crewe1746@naver.com>
2023-04-24 15:49:29 +0900

    chapter05 - send direct message through http

--- chapter05/history/src/index.js
+++ chapter05/history/src/index.js
@@ -1,11 +1,73 @@
 const express = require("express");
+const mongodb = require("mongodb");
 
-const setupHandlers = (app) => {};
+const DB_HOST = process.env.DB_HOST;
+const DB_NAME = process.env.DB_NAME;
 
-const startHttpServer = () => {
+if (!DB_HOST) {
+  throw new Error(
+    "Please specify the host of the MongoDB database with the environment variable DB_HOST."
+  );
+}
+
+if (!DB_NAME) {
+  throw new Error(
+    "Please specify the name of the MongoDB database with the environment variable DB_NAME."
+  );
+}
+
+const connectDB = async () => {
+  const client = await mongodb.MongoClient.connect(DB_HOST);
+  return client.db(DB_NAME);
+};
+
+const setupHandlers = (app, db) => {
+  const videosCollection = db.collection("videos");
+
+  app.post("/viewed", async (req, res) => {
+    const videoPath = req.body.videoPath;
+
+    try {
+      const result = await videosCollection.insertOne({ videoPath: videoPath });
+
+      if (result && result.acknowledged) {
+        res.status(201).send();
+      } else {
+        throw new Error("Failed to insert document");
+      }
+    } catch (err) {
+      console.error(`Error adding video ${videoPath} to history.`);
+      console.error((err && err.stack) || err);
+      res.status(500).send();
+    }
+  });
+
+  app.get("/history", async (req, res) => {
+    const skip = parseInt(req.query.skip);
+    const limit = parseInt(req.query.limit);
+
+    try {
+      const documents = await videosCollection
+        .find()
+        .skip(skip)
+        .limit(limit)
+        .toArray();
+
+      res.status(200).json({ history: documents });
+    } catch (err) {
+      console.error(`Error retrieving history from database.`);
+      console.error((err && err.stack) || err);
+      res.sendStatus(500);
+    }
+  });
+};
+
+const startHttpServer = (db) => {
   return new Promise((resolve) => {
     const app = express();
-    setupHandlers(app);
+    app.use(express.json());
+    setupHandlers(app, db);
+
     const port = (process.env.PORT && parseInt(process.env.PORT)) || 3000;
     app.listen(port, () => {
       resolve();
@@ -14,7 +76,9 @@ const startHttpServer = () => {
 };
 
 const main = () => {
-  return startHttpServer();
+  return connectDB().then((db) => {
+    return startHttpServer(db);
+  });
 };
 
 main()
--- chapter05/video-streaming/src/index.js
+++ chapter05/video-streaming/src/index.js
@@ -1,33 +1,83 @@
 const express = require("express");
 const fs = require("fs");
+const http = require("http");
 
-const app = express();
+const port = process.env.PORT;
 
-if (!process.env.PORT) {
+if (!port) {
   throw new Error(
     "Please specify the port number for the HTTP server with the environment variable PORT."
   );
 }
 
-const port = process.env.PORT;
+const sendViewdMessage = (videoPath) => {
+  const postOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
 
-app.get("/video", (req, res) => {
-  const path = "./videos/SampleVideo_1280x720_1mb.mp4";
+  const requestBody = {
+    videoPath: videoPath,
+  };
 
-  fs.stat(path, (err, stats) => {
-    if (err) {
-      console.log(err);
-      return res.status(500).send(err.message);
-    }
+  const req = http.request("http://history/viewed", postOptions);
 
-    res.writeHead(200, {
-      "Content-Length": stats.size,
-      "Content-Type": "video/mp4",
-    });
-    fs.createReadStream(path).pipe(res);
+  req.on("close", () => {
+    console.log("Sent 'viewed' message to history microservice.");
   });
-});
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+  req.on("error", (err) => {
+    console.error("Failed to send 'viewed' message!");
+    console.error((err && err.stack) || err);
+  });
+
+  req.write(JSON.stringify(requestBody));
+  req.end();
+};
+
+const setupHandlers = (app) => {
+  app.get("/video", (req, res) => {
+    const path = "./videos/SampleVideo_1280x720_1mb.mp4";
+
+    fs.stat(path, (err, stats) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).send(err.message);
+      }
+
+      res.writeHead(200, {
+        "Content-Length": stats.size,
+        "Content-Type": "video/mp4",
+      });
+      fs.createReadStream(path).pipe(res);
+
+      sendViewdMessage(path);
+    });
+  });
+};
+
+const startHttpServer = () => {
+  return new Promise((resolve) => {
+    const app = express();
+    setupHandlers(app);
+
+    app.listen(port, () => {
+      resolve();
+    });
+  });
+};
+
+const main = () => {
+  return startHttpServer();
+};
+
+main()
+  .then(() => {
+    console.log(`Listening on port ${port}`);
+  })
+  .catch((err) => {
+    console.error("Microservice failed to start.");
+    console.error((err && err.stack) || err);
+  });
